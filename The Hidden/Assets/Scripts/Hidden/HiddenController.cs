0d0faf6e8eab8792bfaf238def2fb7781cdc2918
E-Ward <s182964@ucs.ac.uk>
2019-11-14 04:04:00 +0000

    Added sonic alarms, knife model, stamina bar that works!

--- The Hidden/Assets/Scripts/Hidden/HiddenController.cs
+++ The Hidden/Assets/Scripts/Hidden/HiddenController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class HiddenController : MonoBehaviour
@@ -19,6 +20,8 @@ public class HiddenController : MonoBehaviour
 
     public float hiddenHealth;
     public float hiddenStamina;
+    public float hiddenMaxStamina = 100f;
+    public Slider hiddenStaminaSlider;
     public int attackDamage;
 
     public bool isWallClinging;
@@ -46,6 +49,9 @@ public class HiddenController : MonoBehaviour
         hiddenHealth = 100f;
         hiddenStamina = 100f;
 
+        hiddenStaminaSlider.maxValue = hiddenMaxStamina;
+        hiddenStaminaSlider.value = hiddenMaxStamina;
+
         isWallClinging = false;
     }
 
@@ -59,12 +65,12 @@ public class HiddenController : MonoBehaviour
             Attack();
         }
 
-        if (Input.GetKeyDown(KeyCode.G))//This is when the left mouse button is pressed
+        if (Input.GetKeyDown(KeyCode.G))//This is when the G keyboard button is pressed
         {
             GrenadeThrow();
         }
 
-        if (Input.GetKeyDown(KeyCode.M))//This is when the left mouse button is pressed
+        if (Input.GetKeyDown(KeyCode.M))//This is when the M keyboard button is pressed
         {
             molotovThrow();
         }
@@ -87,10 +93,9 @@ public class HiddenController : MonoBehaviour
             {
                 Debug.Log("Player is Pouncing");
                 Pounce();
-            }
-            
-            
+            } 
         }
+        
 
         
         if(!Physics.Raycast(transform.position, -Vector3.up,distanceToGround + 0.1f) )
@@ -104,7 +109,8 @@ public class HiddenController : MonoBehaviour
 
         if (hiddenStamina < 100 && isWallClinging == false)
         {
-            hiddenStamina += 10 * Time.deltaTime;
+            hiddenStamina += 3 * Time.deltaTime;
+            hiddenStaminaSlider.value = hiddenStamina;
         }
 
         if (hiddenStamina <= 0 && isWallClinging == true)
@@ -117,6 +123,7 @@ public class HiddenController : MonoBehaviour
         if (hiddenStamina > 0 && isWallClinging == true)
         {
             hiddenStamina -= 5 * Time.deltaTime;
+            hiddenStaminaSlider.value = hiddenStamina;
         }
 
     }
@@ -132,11 +139,14 @@ public class HiddenController : MonoBehaviour
             characterController.enabled = true;
             rb.velocity = fpsCam.transform.forward * pounceForce;
             hiddenStamina -= 10f;
+            isWallClinging = false;
+            hiddenStaminaSlider.value = hiddenStamina;
         }
         else if (hiddenStamina > 10 && isGrounded == true)
         {
             rb.velocity = fpsCam.transform.forward * pounceForce;
             hiddenStamina -= 10f;
+            hiddenStaminaSlider.value = hiddenStamina;
         }
 
     }
@@ -176,9 +186,9 @@ public class HiddenController : MonoBehaviour
 
     void WallCling()
     {
-                rb.isKinematic = true;
-                isWallClinging = true;
-                characterController.enabled = false;
+        rb.isKinematic = true;
+        isWallClinging = true;
+        characterController.enabled = false;
     }
 
 
---
+++ The Hidden/Assets/SonicAlarm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonicAlarm : MonoBehaviour
+{
+    public AudioSource Alarm;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "IRIS" || player.gameObject.tag == "Hidden")
+        {
+            Alarm.Play();
+            Alarm.loop = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        
+        Alarm.loop = false;
+    }
+}
